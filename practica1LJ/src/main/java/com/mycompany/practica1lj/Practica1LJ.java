affebe3d22aa4b2b2d3c3f668132adb141227247
Alesso <alessogoldodiego@gmail.com>
2024-08-21 23:57:52 -0600

    primeros Cambios

---
+++ practica1LJ/src/main/java/com/mycompany/practica1lj/Practica1LJ.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1lj;
+
+/**
+ *
+ * @author alesso
+ */
+public class Practica1LJ {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
