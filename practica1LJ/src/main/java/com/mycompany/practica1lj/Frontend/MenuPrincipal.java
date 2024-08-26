31e47ce08149e2c1a25e02daff30ba1dd3e5b61b
unknown <alessogoldodiego@gmail.com>
2024-08-25 23:06:32 -0600

    ultimos cambios lap

--- practica1LJ/src/main/java/com/mycompany/practica1lj/Frontend/MenuPrincipal.java
+++ practica1LJ/src/main/java/com/mycompany/practica1lj/Frontend/MenuPrincipal.java
@@ -4,7 +4,11 @@
  */
 package com.mycompany.practica1lj.Frontend;
 
+import com.mycompany.practica1lj.Backend.AnalizadorOperadores;
 import com.mycompany.practica1lj.Backend.NumeroLinea;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.StyledDocument;
 
 /**
  *
@@ -13,13 +17,33 @@ import com.mycompany.practica1lj.Backend.NumeroLinea;
 public class MenuPrincipal extends javax.swing.JFrame {
 
     private NumeroLinea numero;
+    private AnalizadorOperadores analizador;
+
     /**
      * Creates new form MenuPrincipal
      */
     public MenuPrincipal() {
         initComponents();
-        numero = new NumeroLinea(jTextArea1);
+        numero = new NumeroLinea(txtCodigo);
         jScrollPane2.setRowHeaderView(numero);
+        analizador = new AnalizadorOperadores();
+        txtCodigo.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                analizarCodigo();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                analizarCodigo();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                // No se usa normalmente con JTextArea, ya que es para cambios de atributos de estilo
+            }
+        });
+        textPane.setEnabled(false);
     }
 
     @SuppressWarnings("unchecked")
@@ -27,9 +51,11 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private void initComponents() {
 
         panelSquare = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        textPane = new javax.swing.JTextPane();
         panelTexto = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        txtCodigo = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("MENU PRINCIPAL");
@@ -37,22 +63,30 @@ public class MenuPrincipal extends javax.swing.JFrame {
 
         panelSquare.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
+        jScrollPane1.setViewportView(textPane);
+
         javax.swing.GroupLayout panelSquareLayout = new javax.swing.GroupLayout(panelSquare);
         panelSquare.setLayout(panelSquareLayout);
         panelSquareLayout.setHorizontalGroup(
             panelSquareLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 425, Short.MAX_VALUE)
+            .addGroup(panelSquareLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 407, Short.MAX_VALUE)
+                .addContainerGap())
         );
         panelSquareLayout.setVerticalGroup(
             panelSquareLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 501, Short.MAX_VALUE)
+            .addGroup(panelSquareLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1)
+                .addContainerGap())
         );
 
         panelTexto.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane2.setViewportView(jTextArea1);
+        txtCodigo.setColumns(20);
+        txtCodigo.setRows(5);
+        jScrollPane2.setViewportView(txtCodigo);
 
         javax.swing.GroupLayout panelTextoLayout = new javax.swing.GroupLayout(panelTexto);
         panelTexto.setLayout(panelTextoLayout);
@@ -67,7 +101,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
             panelTextoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelTextoLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 489, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 483, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -79,28 +113,41 @@ public class MenuPrincipal extends javax.swing.JFrame {
                 .addGap(6, 6, 6)
                 .addComponent(panelTexto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(17, 17, 17)
-                .addComponent(panelSquare, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(panelSquare, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(6, 6, 6)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(panelTexto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(panelSquare, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(panelSquare, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(panelTexto, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void numeroFila() {
-    }
+    public void analizarCodigo() {
+        String codigo = txtCodigo.getText();
 
+        // Limpiar el JTextPane antes de mostrar el resultado
+        textPane.setText("");
+
+        // Obtener el StyledDocument del JTextPane
+        StyledDocument doc = textPane.getStyledDocument();
+
+        // Pasar el código al analizador para aplicar los colores
+        analizador.analizarCodigo(codigo, doc);
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea1;
     private javax.swing.JPanel panelSquare;
     private javax.swing.JPanel panelTexto;
+    private javax.swing.JTextPane textPane;
+    private javax.swing.JTextArea txtCodigo;
     // End of variables declaration//GEN-END:variables
 }
